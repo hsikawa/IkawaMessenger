68f112a94931a1019f2d8742c276618e12155564
hsikawa <i.yuki0114@gmail.com>
2016-01-11 23:53:26 +0900

    test

---
+++ TCPMessage/src/TCPMessage.java
@@ -0,0 +1,13 @@
+public class TCPMessage {
+	public static void main(String[] args) {
+		System.out.println("TCPメッセージ開始");
+
+		Runnable tcpServerRunnable = new TCPServer();
+		new Thread(tcpServerRunnable).start();
+
+		Runnable tcpClientRunnable = new TCPClient();
+		new Thread(tcpClientRunnable).start();
+
+
+	}
+}
---
+++ TCPMessage/src/TCPServer.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TCPServer implements Runnable {
+
+	private static final int BUFSIZE = 32;
+
+	@Override
+	public void run() {
+
+		int servPort = 8080;
+		int recvMsgSize;
+		byte[] byteBuffer = new byte[BUFSIZE];
+
+
+		ServerSocket servSock;
+		try {
+			servSock = new ServerSocket(servPort);
+
+			for (;;) {
+				Socket clntSock = servSock.accept();
+				System.out.println("Handing client at"
+						+ clntSock.getInetAddress().getHostAddress()
+						+ "on port " + clntSock.getPort());
+
+				InputStream in = clntSock.getInputStream();
+				OutputStream out = clntSock.getOutputStream();
+				while ((recvMsgSize = in.read(byteBuffer)) != -1) {
+					System.out.println("server:" + new String(byteBuffer));
+					out.write(byteBuffer, 0, recvMsgSize);
+					byteBuffer = new byte[BUFSIZE];
+				}
+				clntSock.close();
+
+			}
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+
+	}
+}
