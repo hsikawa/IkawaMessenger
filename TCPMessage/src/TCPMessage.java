738d076cf39da2fad276ebeb657b0ccc3219b025
hsikawa <i.yuki0114@gmail.com>
2016-02-08 21:18:33 +0900

    TCPMessage 修正

--- TCPMessage/src/TCPMessage.java
+++ TCPMessage/src/TCPMessage.java
@@ -1,9 +1,11 @@
+
 public class TCPMessage {
 	public static void main(String[] args) {
 		System.out.println("TCPメッセージ開始");
 
 		Runnable tcpServerRunnable = new TCPServer();
 		new Thread(tcpServerRunnable).start();
+
 
 		Runnable tcpClientRunnable = new TCPClient();
 		new Thread(tcpClientRunnable).start();
